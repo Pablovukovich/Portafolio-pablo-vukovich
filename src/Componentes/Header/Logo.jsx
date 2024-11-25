c219302a03998d65cf9685ed5d97640333819641
Pablo Vukovich <pablovukovich@gmail.com>
2024-11-25 16:19:03 -0300

    probando imagenes

--- src/Componentes/Header/Logo.jsx
+++ src/Componentes/Header/Logo.jsx
@@ -4,7 +4,7 @@ export const Logo = () => {
   return (
     <>
         <div class=" hidden lg:grid grid-cols-3 grid-rows-3 gap-5 transform rotate-45 perspective-[600px] lg:absolute lg:w-96 lg:h-96 lg:left-[50rem] lg:-top-20   ">
-        <div className="bg-second bg-opacity-50 p-5 text-transparent shadow-[30px_30px_30px_-20px_rgba(0,0,0,0.6)] flex justify-center items-center overflow-hidden animate-[scaleUp_3s_ease-in-out_infinite]"> <img src="./public/4-skill.svg" alt="react-logo" className="transform rotate-[-45deg]" /></div>
+        <div className="bg-second bg-opacity-50 p-5 text-transparent shadow-[30px_30px_30px_-20px_rgba(0,0,0,0.6)] flex justify-center items-center overflow-hidden animate-[scaleUp_3s_ease-in-out_infinite]"> <img src="/imagenes/4-skill.svg" alt="react-logo" className="transform rotate-[-45deg]" /></div>
             <div class="bg-second bg-opacity-50  p-5 text-transparent shadow-[30px_30px_30px_-20px_rgba(0,0,0,0.6)] flex justify-center items-center"></div>
             <div className="bg-second bg-opacity-50 p-5 text-transparent shadow-[30px_30px_30px_-20px_rgba(0,0,0,0.6)] 
                  flex justify-center items-center overflow-hidden animate-[scaleUp_3s_ease-in-out_infinite]"><img src="./public/3-skill.svg" alt="js-logo" className='  transform rotate-[-45deg]' /></div>
